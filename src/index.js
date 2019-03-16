fa21b06ecd7a2bbbfc54e33b05f092878d4c6582
PHitkovich <phitkovich@gmail.com>
2019-03-16 18:13:43 +0300

    feat: add implementation getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -4,4 +4,20 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var couter = 0;
+  var arrayModified = preferences.slice();
+
+  for (var i = 0; i < arrayModified.length; i++) {
+
+    if ((j = arrayModified[i]-1) && (k = arrayModified[j]-1) && (i === arrayModified[k]-1) && (j !== k)) {
+      couter++;
+      arrayModified[i] = null;
+      arrayModified[j] = null;
+      arrayModified[k] = null;
+    } else {
+      arrayModified[i] = null;
+    }
+  }
+  
+  return couter;
 };
